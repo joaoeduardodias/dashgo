bdad07d57a0517c17ae6fce55aa95c36131c13ab
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-02-11 13:59:51 -0300

    feat: create sidebar drawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, useBreakpointValue, IconButton } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SideBarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationNav } from './NotificationNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header(): JSX.Element {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -20,6 +24,16 @@ export function Header(): JSX.Element {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
       <Logo />
       {isWideVersion && <SearchBox />}
 
