5c98571ec968d66ae9132fc6e3e44e48e434b48a
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-02-14 15:52:48 -0300

    feat: add validations in form create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,17 +10,49 @@ import {
   Stack,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/SideBar';
 import { Input } from '../../components/Form/Input';
 
-const CrreateUser: NextPage = function () {
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup
+    .string()
+    .required('Senha obrigatória')
+    .min(6, 'A senha deve conter no mínimo 6 caracteres'),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref('password')], 'As senhas precisam ser iguais!'),
+});
+
+const CreateUser: NextPage = function () {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+  const { errors } = formState;
+  const handleCreateUser: SubmitHandler<FieldValues> = async values => {
+    // eslint-disable-next-line no-promise-executor-return
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Box>
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
-        <Box flex="1" borderRadius={8} bg="gray.800" p={['6', '8']}>
+        <Box
+          as="form"
+          onSubmit={handleSubmit(handleCreateUser)}
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={['6', '8']}
+        >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -28,15 +60,29 @@ const CrreateUser: NextPage = function () {
 
           <Stack spacing="8">
             <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input name="email" label="E-mail" />
+              <Input
+                label="Nome completo"
+                error={errors.name}
+                {...register('name')}
+              />
+              <Input
+                label="E-mail"
+                error={errors.email}
+                {...register('email')}
+              />
             </SimpleGrid>
             <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
-              <Input name="password" type="password" label="Senha" />
               <Input
-                name="password_confirmation"
+                type="password"
+                label="Senha"
+                error={errors.password}
+                {...register('password')}
+              />
+              <Input
                 type="password"
                 label="Confirme sua senha"
+                error={errors.password_confirmation}
+                {...register('password_confirmation')}
               />
             </SimpleGrid>
           </Stack>
@@ -47,7 +93,13 @@ const CrreateUser: NextPage = function () {
                   Cancelar
                 </Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button
+                type="submit"
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
@@ -56,4 +108,4 @@ const CrreateUser: NextPage = function () {
   );
 };
 
-export default CrreateUser;
+export default CreateUser;
